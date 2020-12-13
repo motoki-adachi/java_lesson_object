c62b3f30b3277a3d3d7efb832f4b59f2fee1737e
motoki-adachi <zyx.wvu43@icloud.com>
2020-12-13 23:59:03 +0900

    Update Product.java

--- ch01/src/sample/Product.java
+++ ch01/src/sample/Product.java
@@ -4,6 +4,20 @@ public class Product {
 
   private String number;
   private String name;
+  private int peice;
+  private LocalDate sate;
+  private boolean stock;
+
+
+  public Product(String number, String name, int peice, LocalDate sate, boolean stock) {
+    this.number = number;
+    this.name = name;
+    this.peice = peice;
+    this.sate = sate;
+    this.stock = stock;
+  }
+
   
+
   
 }
