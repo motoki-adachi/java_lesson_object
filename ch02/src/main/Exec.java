9f5d2cb662572f280f6820be6980f76b50be65bf
motoki-adachi <zyx.wvu43@icloud.com>
2020-12-26 20:54:44 +0900

    lesson

--- ch02/src/main/Exec.java
+++ ch02/src/main/Exec.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import java.time.LocalDate;
 
--- ch02/src/main/ExecSetter.java
+++ ch02/src/main/ExecSetter.java
@@ -1,9 +1,9 @@
-package sample;
+package main;
 import java.time.LocalDate;
 
 public class ExecSetter {
 	public static void main(String[] args) {
-		Product p1 = new Product("A100", "XenPad", 
+		Product p1 = new Product("A100", "XenPad",
 						 35760, LocalDate.of(2016, 9, 16), true);
 		p1.setPrice(40200);
 		System.out.println("price: " + p1.getPrice());
